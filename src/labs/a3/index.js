af52b64436aca42926a029d7889e0caafa99b2fc
businessmajor <barrios.st@northeastern.edu>
2023-05-31 00:00:38 -0400

    bugs

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -1,9 +1,11 @@
 import JavaScript from "./java-script";
+import Classes from "./classes";
 
 function Assignment3() {
   return (
     <div>
       <h1>Assignment 3</h1>
+      <Classes/>
       <JavaScript />
     </div>
   );
