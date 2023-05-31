a45e4113828216c729973b7b9753447f490cfdba
businessmajor <barrios.st@northeastern.edu>
2023-05-31 00:39:48 -0400

    who to follow

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,6 +1,12 @@
 import { Routes, Route } from "react-router";
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
+import HomeScreen from "./home-screen"
+import ExploreScreen from "./explore-screen"
+import BookmarksScreen from "./bookmarks-screen"
+import ProfileScreen from "./profile-screen"
+import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+
 
 
 function Tuiter() {
@@ -12,14 +18,15 @@ function Tuiter() {
          <NavigationSidebar />
        </div>
        <div className="col-7">
-         <Routes>
-           <Route path="/home" element={<h1>Home</h1>} />
-           <Route path="/explore" element={<h1>Explore</h1>} />
-           <Route path="/notifications" element={<h1>Notifications</h1>}/>
-         </Routes>
+       <Routes>
+         <Route path="/home" element={<HomeScreen />} />
+         <Route path="/explore" element={<ExploreScreen />} />
+         <Route path="/bookmarks" element={<BookmarksScreen />} />
+         <Route path="/profile" element={<ProfileScreen />} />
+      </Routes>
        </div>
        <div className="col-3">
-         <h1>Who to follow</h1>
+       <WhoToFollowListItem />
        </div>
      </div>
    </div>
