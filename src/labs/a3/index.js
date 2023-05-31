f90f1d490d0fb8da5fab73d89a355fe08baa2e42
businessmajor <barrios.st@northeastern.edu>
2023-05-31 00:19:26 -0400

    lab fully debugged

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -2,11 +2,13 @@ import JavaScript from "./java-script";
 import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
 
 function Assignment3() {
   return (
     <div>
       <h1>Assignment 3</h1>
+      <TodoList/>
       <ConditionalOutput/>
       <Styles/>
       <Classes/>
