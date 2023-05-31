46d929e4709991eead1400ebb37999bbec9d3cce
businessmajor <barrios.st@northeastern.edu>
2023-05-31 00:15:23 -0400

    finished 2.6

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -1,10 +1,14 @@
 import JavaScript from "./java-script";
 import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
 
 function Assignment3() {
   return (
     <div>
       <h1>Assignment 3</h1>
+      <ConditionalOutput/>
+      <Styles/>
       <Classes/>
       <JavaScript />
     </div>
