6e74631704f7ee210e3fc22c82379cc71da96223
businessmajor <barrios.st@northeastern.edu>
2023-05-31 01:56:31 -0400

    netlify branches failing to compile?

--- src/tuiter/explore-screen/explore-screen.js
+++ src/tuiter/explore-screen/explore-screen.js
@@ -3,19 +3,20 @@ import TuitSummaryList from "../tuit-summary-list";
 import { AiOutlineSearch } from "react-icons/ai";
 import { GoGear } from "react-icons/go";
 import "./index.css";
+
 function ExploreScreen() {
   return (
     <>
       <div className="row">
         <div className="col-11 position-relative">
-          <input placeholder="Search Tuiter"
-            className="form-control rounded-pill ps-5" />
-          <AiOutlineSearch className="fs-3 position-absolute 
-                       wd-nudge-up"/>
+          <input
+            placeholder="Search Tuiter"
+            className="form-control rounded-pill ps-5"
+          />
+          <AiOutlineSearch className="fs-3 position-absolute wd-nudge-up" />
         </div>
         <div className="col-1">
-          <GoGear className="wd-top-4 float-end
-                       fs-3 position-relative"/>
+          <GoGear className="wd-top-4 float-end fs-3 position-relative" />
         </div>
       </div>
       <ul className="nav nav-pills mb-2 mt-2">
@@ -30,12 +31,12 @@ function ExploreScreen() {
         </li>
       </ul>
       <div className="position-relative mb-2">
-        <img src={require('../images/spaceship.jpg') } className="w-100" />
-        <h1 className="position-absolute wd-nudge-up text-white">
-          SpaceX Starship</h1>
+        <img src={require('../images/spaceship.jpg')} className="w-100" alt="SpaceX Starship" />
+        <h1 className="position-absolute wd-nudge-up text-white">SpaceX Starship</h1>
       </div>
       <TuitSummaryList />
     </>
   );
-};
+}
+
 export default ExploreScreen;
